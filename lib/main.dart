f69597cffbd2e5006ba1a285dc0b1ab3ebc14c54
russel0907 <russel7ras@gmail.com>
2023-10-04 14:36:01 +0800

    add login page

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:anime_rush/screen/login_page.dart.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -13,6 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
+        useMaterial3: true,
         textTheme: GoogleFonts.fredokaTextTheme(
           ThemeData(brightness: Brightness.dark).textTheme.copyWith(
                 bodyLarge: const TextStyle(color: Colors.white),
@@ -23,165 +25,7 @@ class MyApp extends StatelessWidget {
               ),
         ),
       ),
-      home: const MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  bool isChecked = false;
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: const Color(0xff121315),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          SizedBox(
-            height: MediaQuery.of(context).size.height * 0.1,
-          ),
-          Image.asset(
-            'assets/images/icon.png',
-            width: 150,
-            height: 150,
-          ),
-          Container(
-            height: MediaQuery.of(context).size.height * 0.6,
-            decoration: const BoxDecoration(
-              color: Color(0xff1F2022),
-              borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(20.0),
-                topRight: Radius.circular(20.0),
-              ),
-            ),
-            child: Padding(
-              padding: const EdgeInsets.only(
-                top: 30.0,
-                left: 30,
-                right: 30,
-              ),
-              child: Column(
-                children: [
-                  Container(
-                    decoration: BoxDecoration(
-                      color: const Color(0xff121315),
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    padding: const EdgeInsets.all(15),
-                    margin: const EdgeInsets.all(10),
-                    child: TextFormField(
-                      decoration: const InputDecoration.collapsed(
-                        hintText: 'Full Name',
-                        hintStyle: TextStyle(
-                          color: Colors.white,
-                        ),
-                      ),
-                    ),
-                  ),
-                  Container(
-                    decoration: BoxDecoration(
-                      color: const Color(0xff121315),
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    padding: const EdgeInsets.all(15),
-                    margin: const EdgeInsets.all(10),
-                    child: TextFormField(
-                      decoration: const InputDecoration.collapsed(
-                        hintText: 'Username',
-                        hintStyle: TextStyle(
-                          color: Colors.white,
-                        ),
-                      ),
-                    ),
-                  ),
-                  Container(
-                    decoration: BoxDecoration(
-                      color: const Color(0xff121315),
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    padding: const EdgeInsets.all(15),
-                    margin: const EdgeInsets.all(10),
-                    child: TextFormField(
-                      decoration: const InputDecoration.collapsed(
-                        hintText: 'Password',
-                        hintStyle: TextStyle(
-                          color: Colors.white,
-                        ),
-                      ),
-                    ),
-                  ),
-                  Container(
-                    decoration: BoxDecoration(
-                      color: const Color(0xff121315),
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    padding: const EdgeInsets.all(15),
-                    margin: const EdgeInsets.all(10),
-                    child: TextFormField(
-                      decoration: const InputDecoration.collapsed(
-                        hintText: 'User name',
-                        hintStyle: TextStyle(
-                          color: Colors.white,
-                        ),
-                      ),
-                    ),
-                  ),
-                  Container(
-                    margin: const EdgeInsets.only(top: 10, left: 10, right: 10),
-                    child: Row(
-                      children: [
-                        const Text('Remember Me'),
-                        Checkbox(
-                            value: isChecked,
-                            onChanged: (bool? value) {
-                              setState(() {
-                                isChecked = value!;
-                              });
-                            })
-                      ],
-                    ),
-                  ),
-                  Container(
-                    width: MediaQuery.of(context).size.width * 0.9,
-                    decoration: const BoxDecoration(
-                      color: Color(0xff398AD9),
-                      borderRadius: BorderRadius.all(Radius.circular(10.0)),
-                    ),
-                    margin: const EdgeInsets.all(10),
-                    child: TextButton(
-                      onPressed: () {},
-                      child: const Text(
-                        'SIGN UP',
-                        style: TextStyle(color: Colors.white, fontSize: 22),
-                      ),
-                    ),
-                  ),
-                  Container(
-                    width: MediaQuery.of(context).size.width * 0.9,
-                    margin: const EdgeInsets.all(10),
-                    child: TextButton(
-                      onPressed: () {},
-                      child: const Text(
-                        'Already have an account ?',
-                        style: TextStyle(color: Colors.white, fontSize: 18),
-                        textAlign: TextAlign.start,
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          )
-        ],
-      ),
+      home: const LoginPage(),
     );
   }
 }
