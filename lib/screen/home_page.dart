c4f99cddfa2b31dbf9c9b7ac51851d9b91a38db3
russel0907 <russel7ras@gmail.com>
2023-10-05 16:59:43 +0800

    add home page

---
+++ lib/screen/home_page.dart
@@ -0,0 +1,234 @@
+import 'package:flutter/material.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _selectedIndex = 0;
+
+  final String getMediaTrend = '''
+    query GetMediaTrend(\$page: Int, \$perPage: Int) {
+      Page(page: \$page, perPage: \$perPage) {
+        pageInfo {
+          total
+          currentPage
+          lastPage
+          hasNextPage
+          perPage
+        }
+        media {
+          id
+          title {
+            romaji
+          }
+          coverImage {
+            large
+            }
+        }
+      }
+    }
+  ''';
+
+  var variables = {
+    'page': 1,
+    'perPage': 10,
+  };
+
+  final List<Widget> _pages = [
+    // For example: Page1(), Page2(), Page3(), ...
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        backgroundColor: const Color(0xFF1F2022),
+        leading: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Image.asset('assets/images/icon-2.png'),
+        ),
+        actions: const <Widget>[
+          Padding(
+            padding: EdgeInsets.all(8.0),
+            child: CircleAvatar(
+              radius: 20.0,
+              backgroundImage: AssetImage('assets/profile_pic.png'),
+            ),
+          ),
+        ],
+        title: const SizedBox.shrink(),
+        bottom: PreferredSize(
+            preferredSize: const Size.fromHeight(130.0),
+            child: Container(
+              margin: EdgeInsets.only(
+                  bottom: MediaQuery.of(context).size.height * 0.075),
+              width: MediaQuery.of(context).size.width * 0.9,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(12.0),
+                child: Container(
+                  color: const Color(0xff121315),
+                  height: MediaQuery.of(context).size.height * 0.06,
+                  child: const TextField(
+                    decoration: InputDecoration(
+                      contentPadding: EdgeInsets.only(left: 16.0),
+                      hintText: 'Search',
+                      hintStyle: TextStyle(color: Colors.white),
+                      border: InputBorder.none,
+                    ),
+                  ),
+                ),
+              ),
+            )),
+      ),
+      body: Container(
+        color: const Color(0xff121315),
+        child: Query(
+          options: QueryOptions(
+            document: gql(getMediaTrend),
+            variables: variables,
+          ),
+          builder: (
+            QueryResult result, {
+            Future<QueryResult?> Function(FetchMoreOptions)? fetchMore,
+            Future<QueryResult?> Function()? refetch,
+          }) {
+            if (result.hasException) {
+              return Text('Error: ${result.exception.toString()}');
+            }
+
+            if (result.isLoading) {
+              return const Center(child: CircularProgressIndicator());
+            }
+            final mediaList = result.data?['Page']['media'];
+
+            return SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  const Padding(
+                    padding: EdgeInsets.only(left: 16.0, right: 16, top: 16),
+                    child: Text(
+                      'Trending Now',
+                      style: TextStyle(color: Color(0xff8F8F8F), fontSize: 16),
+                    ),
+                  ),
+                  if (mediaList != null)
+                    GridView.builder(
+                      gridDelegate:
+                          const SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 2,
+                        childAspectRatio: 0.7,
+                      ),
+                      itemCount: mediaList.length,
+                      shrinkWrap: true,
+                      physics: const NeverScrollableScrollPhysics(),
+                      itemBuilder: (context, index) {
+                        final media = mediaList[index];
+                        return Padding(
+                          padding: const EdgeInsets.all(16.0),
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(12.0),
+                            child: Stack(
+                              children: [
+                                Container(
+                                  height:
+                                      MediaQuery.of(context).size.height * 0.8,
+                                  width:
+                                      MediaQuery.of(context).size.width * 0.5,
+                                  child: Image.network(
+                                    media['coverImage']['large'],
+                                    fit: BoxFit.fitWidth,
+                                  ),
+                                ),
+                                Positioned.fill(
+                                  child: Container(
+                                    decoration: BoxDecoration(
+                                      gradient: LinearGradient(
+                                        begin: Alignment.bottomCenter,
+                                        end: Alignment.topCenter,
+                                        colors: [
+                                          Colors.black.withOpacity(0.7),
+                                          Colors.transparent,
+                                        ],
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                                Positioned(
+                                  bottom: 10,
+                                  left: 10,
+                                  child: Row(
+                                    children: [
+                                      Container(
+                                        height: 40,
+                                        width: 3,
+                                        color: const Color(0xff398AD9),
+                                      ),
+                                      Container(
+                                        padding:
+                                            const EdgeInsets.only(left: 10),
+                                        width:
+                                            MediaQuery.of(context).size.width *
+                                                0.36,
+                                        child: Text(
+                                            'Romaji: ${media['title']['romaji']}'),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        );
+                      },
+                    ),
+                ],
+              ),
+            );
+          },
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: const Color(0xFF1F2022),
+        type: BottomNavigationBarType.fixed,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.notifications,
+              color: Color(0xFFDBDBDB),
+            ),
+            activeIcon: Icon(Icons.notifications, color: Color(0xFF398AD9)),
+            label: '',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home, color: Color(0xFFDBDBDB)),
+            activeIcon: Icon(Icons.home, color: Color(0xFF398AD9)),
+            label: '',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.bookmark, color: Color(0xFFDBDBDB)),
+            activeIcon: Icon(Icons.bookmark, color: Color(0xFF398AD9)),
+            label: '',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: const Color(0xFF398AD9),
+        iconSize: 36,
+        onTap: _onItemTapped,
+        useLegacyColorScheme: true,
+      ),
+    );
+  }
+}
