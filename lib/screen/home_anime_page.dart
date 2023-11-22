e6fa32cc7e46be1d8ad5997b6df84513c34d032f
Russel Rosmin <russel7ras@gmail.com>
2023-11-22 13:42:16 +0800

    update query anime content by click next page

--- lib/screen/home_anime_page.dart
+++ lib/screen/home_anime_page.dart
@@ -3,11 +3,9 @@ import 'package:flutter/material.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
 
 class HomeAnimeTabPage extends StatefulWidget {
-  final String getMediaTrend;
   final Map<String, dynamic> variables;
 
   const HomeAnimeTabPage({
-    required this.getMediaTrend,
     required this.variables,
     Key? key,
   }) : super(key: key);
@@ -17,13 +15,64 @@ class HomeAnimeTabPage extends StatefulWidget {
 }
 
 class _HomeAnimeTabPageState extends State<HomeAnimeTabPage> {
+  int currentPage = 1; // Initial page number
+  int itemsPerPage = 20; // Number of items to fetch per page
+  bool isLoadingMore = false; // To prevent multiple requests while loading
+
   @override
   Widget build(BuildContext context) {
     return Container(
       color: const Color(0xff121315),
       child: Query(
         options: QueryOptions(
-          document: gql(widget.getMediaTrend),
+          document: gql('''
+          {
+            Page(page: $currentPage, perPage: $itemsPerPage) {
+              media(format: TV) {
+                id
+                description
+                title {
+                  romaji
+                  english
+                  native
+                  userPreferred
+                }
+                format
+                coverImage {
+                  large
+                  medium
+                }
+                bannerImage
+                season
+                seasonInt
+                seasonYear
+                episodes
+                status
+                volumes
+                chapters
+                nextAiringEpisode {
+                  id
+                  episode
+                  timeUntilAiring
+                  media {
+                    title {
+                      romaji
+                      english
+                      native
+                      userPreferred
+                    }
+                  }
+                }
+                streamingEpisodes {
+                  title
+                  thumbnail
+                  url
+                  site
+                }
+              }
+            }
+          }
+          '''),
           variables: widget.variables,
         ),
         builder: (
@@ -38,6 +87,7 @@ class _HomeAnimeTabPageState extends State<HomeAnimeTabPage> {
           if (result.isLoading) {
             return const Center(child: CircularProgressIndicator());
           }
+
           final mediaList = result.data?['Page']['media'];
 
           return SingleChildScrollView(
@@ -65,79 +115,169 @@ class _HomeAnimeTabPageState extends State<HomeAnimeTabPage> {
                     shrinkWrap: true,
                     physics: const NeverScrollableScrollPhysics(),
                     itemBuilder: (context, index) {
-                      final media = mediaList[index];
-                      return GestureDetector(
-                        onTap: () {
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) =>
-                                      AnimePage(media: media)));
-                        },
-                        child: Padding(
-                          padding: const EdgeInsets.all(16.0),
-                          child: ClipRRect(
-                            borderRadius: BorderRadius.circular(12.0),
-                            child: Stack(
-                              children: [
-                                Container(
-                                  height:
-                                      MediaQuery.of(context).size.height * 0.8,
-                                  width:
-                                      MediaQuery.of(context).size.width * 0.5,
-                                  child: Image.network(
-                                    media['coverImage']['large'],
-                                    fit: BoxFit.fitWidth,
+                      if (index < mediaList.length) {
+                        final media = mediaList[index];
+                        return GestureDetector(
+                          onTap: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) =>
+                                        AnimePage(media: media)));
+                          },
+                          child: Padding(
+                            padding: const EdgeInsets.all(16.0),
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.circular(12.0),
+                              child: Stack(
+                                children: [
+                                  SizedBox(
+                                    height: MediaQuery.of(context).size.height *
+                                        0.8,
+                                    width:
+                                        MediaQuery.of(context).size.width * 0.5,
+                                    child: Image.network(
+                                      media['coverImage']['large'],
+                                      fit: BoxFit.fitWidth,
+                                    ),
                                   ),
-                                ),
-                                Positioned.fill(
-                                  child: Container(
-                                    decoration: BoxDecoration(
-                                      gradient: LinearGradient(
-                                        begin: Alignment.bottomCenter,
-                                        end: Alignment.topCenter,
-                                        colors: [
-                                          Colors.black.withOpacity(0.7),
-                                          Colors.transparent,
-                                        ],
+                                  Positioned.fill(
+                                    child: Container(
+                                      decoration: BoxDecoration(
+                                        gradient: LinearGradient(
+                                          begin: Alignment.bottomCenter,
+                                          end: Alignment.topCenter,
+                                          colors: [
+                                            Colors.black.withOpacity(0.7),
+                                            Colors.transparent,
+                                          ],
+                                        ),
                                       ),
                                     ),
                                   ),
-                                ),
-                                Positioned(
-                                  bottom: 10,
-                                  left: 10,
-                                  child: Row(
-                                    children: [
-                                      Container(
-                                        height: 40,
-                                        width: 3,
-                                        color: const Color(0xff398AD9),
-                                      ),
-                                      Container(
-                                        padding:
-                                            const EdgeInsets.only(left: 10),
-                                        width:
-                                            MediaQuery.of(context).size.width *
-                                                0.36,
-                                        child:
-                                            Text('${media['title']['romaji']}'),
-                                      ),
-                                    ],
+                                  Positioned(
+                                    bottom: 10,
+                                    left: 10,
+                                    child: Row(
+                                      children: [
+                                        Container(
+                                          height: 40,
+                                          width: 3,
+                                          color: const Color(0xff398AD9),
+                                        ),
+                                        Container(
+                                          padding:
+                                              const EdgeInsets.only(left: 10),
+                                          width: MediaQuery.of(context)
+                                                  .size
+                                                  .width *
+                                              0.36,
+                                          child: Text(
+                                              '${media['title']['romaji']}'),
+                                        ),
+                                      ],
+                                    ),
                                   ),
-                                ),
-                              ],
+                                ],
+                              ),
                             ),
                           ),
-                        ),
-                      );
+                        );
+                      } else if (isLoadingMore) {
+                        return const Center(child: CircularProgressIndicator());
+                      } else {
+                        return const Center(child: Text('No More Content'));
+                      }
                     },
                   ),
+                if (!isLoadingMore && mediaList != null && mediaList.length > 0)
+                  GestureDetector(
+                      onTap: () {
+                        loadNextPage();
+                      },
+                      child: Center(
+                          child: Padding(
+                        padding: EdgeInsets.only(
+                            top: MediaQuery.of(context).size.height * 0.04,
+                            bottom: MediaQuery.of(context).size.height * 0.06),
+                        child: const Text(
+                          'Next Page',
+                          style: TextStyle(fontSize: 17),
+                        ),
+                      ))),
               ],
             ),
           );
         },
       ),
     );
+  }
+
+  void loadNextPage() {
+    if (!isLoadingMore) {
+      setState(() {
+        isLoadingMore = true;
+      });
+
+      final options = QueryOptions(
+        document: gql('''
+      {
+        Page(page: $currentPage, perPage: $itemsPerPage) {
+          media(format: TV) {
+            id
+            description
+            title {
+              romaji
+              english
+              native
+              userPreferred
+            }
+            format
+            coverImage {
+              large
+              medium
+            }
+            bannerImage
+            season
+            seasonInt
+            seasonYear
+            episodes
+            status
+            volumes
+            chapters
+            nextAiringEpisode {
+              id
+              episode
+              timeUntilAiring
+              media {
+                title {
+                  romaji
+                  english
+                  native
+                  userPreferred
+                }
+              }
+            }
+            streamingEpisodes {
+              title
+              thumbnail
+              url
+              site
+            }
+          }
+        }
+      }
+      '''),
+        variables: widget.variables,
+      );
+
+      GraphQLClient client = GraphQLProvider.of(context).value;
+      client.query(options).then((newResult) {
+        setState(() {
+          isLoadingMore = false;
+          currentPage++;
+        });
+      });
+    }
   }
 }
