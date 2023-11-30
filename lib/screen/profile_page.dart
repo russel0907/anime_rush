2bb34451f3288a22d0ba30f02148dc1271ca2940
Russel Rosmin <russel7ras@gmail.com>
2023-11-30 11:59:06 +0800

    update profile page

--- lib/screen/profile_page.dart
+++ lib/screen/profile_page.dart
@@ -1,5 +1,9 @@
+import 'package:anime_rush/screen/in_progress_anime_page.dart';
+import 'package:anime_rush/screen/my_movies_anime_page.dart';
+import 'package:anime_rush/screen/saved_anime_page.dart';
 import 'package:flutter/material.dart';
-import 'package:url_launcher/url_launcher.dart';
+
+import 'my_activities_anime_page.dart';
 
 class ProfilePage extends StatefulWidget {
   const ProfilePage({
@@ -10,8 +14,10 @@ class ProfilePage extends StatefulWidget {
   State<ProfilePage> createState() => _ProfilePageState();
 }
 
-class _ProfilePageState extends State<ProfilePage> {
+class _ProfilePageState extends State<ProfilePage>
+    with SingleTickerProviderStateMixin {
   int _selectedIndex = 0;
+  late TabController _tabController;
 
   void _onItemTapped(int index) {
     setState(() {
@@ -22,6 +28,7 @@ class _ProfilePageState extends State<ProfilePage> {
   @override
   void initState() {
     super.initState();
+    _tabController = TabController(length: 4, vsync: this);
   }
 
   @override
@@ -35,7 +42,7 @@ class _ProfilePageState extends State<ProfilePage> {
       backgroundColor: const Color(0xff121315),
       appBar: AppBar(
         automaticallyImplyLeading: false,
-        backgroundColor: const Color(0xFF1F2022),
+        backgroundColor: const Color(0xff121315),
         leading: Padding(
           padding: const EdgeInsets.all(8.0),
           child: Image.asset('assets/images/icon-2.png'),
@@ -52,26 +59,95 @@ class _ProfilePageState extends State<ProfilePage> {
         title: const SizedBox.shrink(),
       ),
       body: SingleChildScrollView(
-        child: Stack(
-          children: [
-            Container(
-              color: const Color(0xFF1F2022),
-              height: MediaQuery.of(context).size.height * 0.15,
-              width: MediaQuery.of(context).size.width,
+          child: Stack(
+        children: [
+          Column(
+            children: [
+              Container(
+                color: const Color(0xff121315),
+                height: MediaQuery.of(context).size.height * 0.15,
+              ),
+              Container(
+                padding: const EdgeInsets.all(20),
+                color: const Color(0xFF1F2022),
+                height: MediaQuery.of(context).size.height * 0.15,
+                child: const Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Column(
+                          children: [
+                            Text(
+                              'Followers',
+                              style: TextStyle(color: Color(0xff398AD9)),
+                            ),
+                            Text('777'),
+                          ],
+                        ),
+                        Column(
+                          children: [
+                            Text(
+                              'Following',
+                              style: TextStyle(color: Color(0xff398AD9)),
+                            ),
+                            Text('200'),
+                          ],
+                        )
+                      ],
+                    ),
+                    Text("Levi34"),
+                    Text("Berlin, Germany"),
+                  ],
+                ),
+              ),
+              Column(
+                children: [
+                  Container(
+                    padding: const EdgeInsets.all(20),
+                    color: const Color(0xff121315),
+                    child: TabBar(
+                      dividerColor: Colors.transparent,
+                      indicatorColor: const Color(0xff398AD9),
+                      labelColor: const Color(0xff398AD9),
+                      controller: _tabController,
+                      tabs: const [
+                        Tab(text: 'Saved Anime'),
+                        Tab(text: 'In Progress'),
+                        Tab(text: 'Movies'),
+                        Tab(text: 'Activities'),
+                      ],
+                    ),
+                  ),
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height * 0.05,
+                    width: MediaQuery.of(context).size.width,
+                    child: TabBarView(
+                      controller: _tabController,
+                      children: const [
+                        SavedAnimePage(),
+                        InProgressAnimePage(),
+                        MyMoviesAnimePage(),
+                        MyActivitiesAnimePage()
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+          Positioned(
+            top: MediaQuery.of(context).size.height * 0.15 - 50,
+            left: MediaQuery.of(context).size.width / 2 - 50,
+            child: Image.asset(
+              'assets/images/circle-profile.png',
+              width: 100,
+              height: 100,
             ),
-            Column(
-              children: [
-                SizedBox(
-                  height: MediaQuery.of(context).size.height * 0.05,
-                ),
-                SizedBox(
-                  height: MediaQuery.of(context).size.height * 0.02,
-                ),
-              ],
-            )
-          ],
-        ),
-      ),
+          ),
+        ],
+      )),
       bottomNavigationBar: BottomNavigationBar(
         backgroundColor: const Color(0xFF1F2022),
         type: BottomNavigationBarType.fixed,
